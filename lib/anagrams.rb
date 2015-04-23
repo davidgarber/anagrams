ee2c17b9726b21b0264cc0054d2d0f061facaedf
Leon Zagorouiko & David Jonathan Garber <student@epicodus.com>
2015-04-23 15:52:33 -0700

    Changed triple argument to single argument passes test

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,6 +1,6 @@
 class String
-  define_method(:anagrams) do |arg1, arg2, arg3|
-    sorted_args = [arg1.chars.sort.join, arg2.chars.join, arg3.chars.join]
+  define_method(:anagrams) do |arg1|
+    sorted_args = [arg1.chars.sort.join]
 
     phrase = self
     phrase_words = phrase.split(' ')
@@ -12,6 +12,8 @@ class String
 
       if sorted_args.include?(sorted_word)
         output.push(word)
+
+      elsif output.push("no anagram")
       end
     end
     output
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,10 +3,14 @@ require('anagrams')
 
 describe('String#anagrams') do
   it("Inputs a and chooses a, b, c returns a") do
-    expect("a".anagrams("a","b","c")).to(eq(["a"]))
+    expect("a".anagrams("a")).to(eq(["a"]))
   end
 
   it("Inputs dog and chooses dog, cat, cow returns dog") do
-    expect("dog".anagrams("dog", "cat", "cow")).to(eq(["dog"]))
+    expect("dog".anagrams("dog")).to(eq(["dog"]))
+  end
+
+  it("Inputs dog and chooses cat, cow, snakes returns no anagram") do
+    expect("dog".anagrams("cat")).to(eq(["no anagram"]))
   end
 end
